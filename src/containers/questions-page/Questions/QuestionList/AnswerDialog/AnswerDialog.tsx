caf1ed8da1d19edf1a046226883dabe84a957184
Ijat Ayam <ijat191999@gmail.com>
2024-01-19 20:50:39 +0800

    change to html-to-image for better image generation

--- src/containers/questions-page/Questions/QuestionList/AnswerDialog/AnswerDialog.tsx
+++ src/containers/questions-page/Questions/QuestionList/AnswerDialog/AnswerDialog.tsx
@@ -1,7 +1,7 @@
 import type { Question } from '@prisma/client';
 import Link from 'next/link';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import html2canvas from 'html2canvas';
+import { toPng } from 'html-to-image';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 type Props = {
@@ -36,10 +36,10 @@ export default function AnswerDialog(props: Readonly<Props>) {
   const handleSave = useCallback(() => {
     if (questionRef.current === null) return;
 
-    html2canvas(questionRef.current, { scale: 1 }).then(canvas => {
+    toPng(questionRef.current).then(dataUrl => {
       const link = document.createElement('a');
       link.download = `tanya-koding-${question?.questionId}.png`;
-      link.href = canvas.toDataURL('image/png');
+      link.href = dataUrl;
       link.click();
     });
   }, [question?.questionId]);
@@ -53,7 +53,6 @@ export default function AnswerDialog(props: Readonly<Props>) {
       <DialogContent className="pt-10">
         <div
           className="flex flex-col gap-2 rounded-2xl bg-gradient-to-r from-grad-start to-grad-end p-5"
-          ref={questionRef}
           style={{ zoom: scaleRatio }}
         >
           <header className="flex flex-col items-center gap-2 py-2">
@@ -69,6 +68,27 @@ export default function AnswerDialog(props: Readonly<Props>) {
               oleh <span className="font-bold">Primaxoft</span>
             </p>
           </footer>
+        </div>
+        {/* hidden for html-to-image */}
+        <div className="absolute -left-[1000vw]">
+          <div
+            className="flex w-[29rem] flex-col gap-2 rounded-2xl bg-gradient-to-r from-grad-start to-grad-end p-5"
+            ref={questionRef}
+          >
+            <header className="flex flex-col items-center gap-2 py-2">
+              <p className="text-center text-lg font-semibold text-gray-300">Soalan</p>
+              <hr className="w-1/4 border-gray-300" />
+              <p className="text-xl font-semibold">#TanyaKoding{question?.questionId}</p>
+            </header>
+            <div className="flex min-h-52 items-center justify-center rounded-xl bg-background p-7">
+              <p className="text-center text-xl font-semibold">{question?.question}</p>
+            </div>
+            <footer className="pt-1">
+              <p className="text-center">
+                oleh <span className="font-bold">Primaxoft</span>
+              </p>
+            </footer>
+          </div>
         </div>
         <p className="text-center text-sm">Jangan lupa #TanyaKoding{question?.questionId}</p>
         <div className="flex flex-col justify-end gap-5 pt-8">
